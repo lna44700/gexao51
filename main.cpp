41099ec403a0cc5854da69c38ebee6b07b22e3d9
sylvainmenard <sylv.menard@free.fr>
2016-02-24 14:03:37 +0100

    Sylvain Menard

--- main.cpp
+++ main.cpp
@@ -6,7 +6,8 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     F_Principale w;
     w.show();
-
+vuvgyvh:vgh
+            bubhubhuibninjo*
     int heyhey;
     return a.exec();
 }
