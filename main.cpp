7080e8197d555a2b632abcc972e14c931103d350
sylvainmenard <sylv.menard@free.fr>
2016-02-24 14:52:07 +0100

    Tutoriel

--- main.cpp
+++ main.cpp
@@ -6,8 +6,6 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     F_Principale w;
     w.show();
-vuvgyvh:vgh
-            bubhubhuibninjo*
-    int heyhey;
+    test;
     return a.exec();
 }
