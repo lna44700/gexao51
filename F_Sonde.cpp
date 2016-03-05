445b67962460ac68373d39778a39f40d5df59c1b
sylvainmenard <sylv.menard@free.fr>
2016-03-05 23:17:15 +0100

    Mise à jour suite à erreur dans la librairie

--- F_Sonde.cpp
+++ F_Sonde.cpp
@@ -1,14 +1,62 @@
 #include "F_Sonde.h"
 #include "ui_F_Sonde.h"
+#include <QDate>
+#include <QString>
+#include <QTime>
 
 F_Sonde::F_Sonde(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::F_Sonde)
 {
     ui->setupUi(this);
+
+    QDate date = QDate::currentDate();
+    ui->deDateAcquisition->setDate(date);
+
+    QTime heure = QTime::currentTime();
+    ui->tiHeureAcquisition->setTime(heure);
+
+    ui->leTpsAcquisition->setPlaceholderText("en minutes");
+    ui->leIntervalle->setPlaceholderText("en secondes");
 }
 
 F_Sonde::~F_Sonde()
 {
     delete ui;
+}
+
+void F_Sonde::on_btnModeAcquisition_clicked()
+{
+    QString sIndication("en minutes");
+    if (ui->btnModeAcquisition->text() == "Automatique")
+    {
+        ui->btnModeAcquisition->setText("Manuel");
+        ui->tlTpsAcquisition->hide();
+        ui->leTpsAcquisition->hide();
+        ui->tlIntervalle->hide();
+        ui->leIntervalle->hide();
+        ui->tlDateHeure->hide();
+        ui->deDateAcquisition->hide();
+        ui->tiHeureAcquisition->hide();
+
+    }
+    else if (ui->btnModeAcquisition->text() == "Manuel")
+    {
+        ui->btnModeAcquisition->setText("Automatique");
+        ui->tlTpsAcquisition->show();
+        ui->leTpsAcquisition->show();
+        ui->tlIntervalle->show();
+        ui->leIntervalle->show();
+        ui->tlDateHeure->show();
+        ui->deDateAcquisition->show();
+        ui->tiHeureAcquisition->show();
+    }
+}
+
+void F_Sonde::on_cbModeVisualisation_toggled(bool checked)
+{
+    if (checked == true)
+    {
+        // desactiver le tableau
+    }
 }
--- F_Sonde.h
+++ F_Sonde.h
@@ -15,6 +15,11 @@ public:
     explicit F_Sonde(QWidget *parent = 0);
     ~F_Sonde();
 
+private slots:
+    void on_btnModeAcquisition_clicked();
+
+    void on_cbModeVisualisation_toggled(bool checked);
+
 private:
     Ui::F_Sonde *ui;
 };
